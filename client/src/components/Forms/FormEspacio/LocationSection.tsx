73b1d262df3517e7bd3ac4de3408959956f2318b
Raul Zarza <raulzarza.dev@gmail.com>
2021-10-28 09:33:31 -0600

    hot fix

--- client/src/components/Forms/FormEspacio/LocationSection.tsx
+++ client/src/components/Forms/FormEspacio/LocationSection.tsx
@@ -1,5 +1,4 @@
 import SelectLocation from '@comps/SelectLocation'
-const GOOGLE_MAPS_API_KEY = 'AIzaSyASP-O7EJFd_vqNA27fIAZAAhHf7jAs70o'
 export default function LocationSection() {
   return (
     <div className="">

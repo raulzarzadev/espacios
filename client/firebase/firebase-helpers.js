05ff98e475c17f176e6bbfcb707e6af0daa751a0
Raul Zarza <raulzarza.dev@gmail.com>
2022-01-24 15:16:08 -0700

    deep firebase unformat

--- client/firebase/firebase-helpers.js
+++ client/firebase/firebase-helpers.js
@@ -7,30 +7,10 @@ export const normalizeDoc = (doc) => {
   if (!doc?.exists) return null // The document  not exist
   const data = doc.data()
   const id = doc.id
-
-  const {
-    updatedAt,
-    registryDate,
-    createdAt,
-    date,
-    birth,
-    joinedAt,
-    lastUpdate,
-  } = data
-  const dates = unfierebazeDates({
-    updatedAt,
-    registryDate,
-    joinedAt,
-    createdAt,
-    birth,
-    date,
-    lastUpdate,
-  })
-
+  const res = deepDesfirebaseDates(data)
   return {
     id,
-    ...data,
-    ...dates
+    ...res
   }
 }
 
@@ -47,26 +27,66 @@ export const unfierebazeDates = (dates = {}) => {
   }
   return aux
 }
+const DATE_FIELDS = [
+  'birth',
+  'date',
+  'createdAt',
+  'updatedAt',
+  'finishAt',
+  'startAt',
+  'registryDate',
+  'publishEnds',
+  'publishStart',
+  'lastUpdate'
+]
+export const deepDesfirebaseDates = (object) => {
+  const AUX_OBJ = object
+  Object.keys(object).forEach((key) => {
+    if (DATE_FIELDS.includes(key)) AUX_OBJ[key] = unfierebazeDate(object[key])
+    if (typeof object[key] === 'object') {
+      // ------------------------------ IF IS ARRAY ------------------------------
+      if (Array.isArray(object[key])) {
+        object[key].map((item) => {
+          return deepDesfirebaseDates(item)
+        })
+        
+      } else {
+         deepDesfirebaseDates(object[key])
+      }
+      // ------------------------------ IF IS OBJECT ------------------------------
+    }
+  })
+  return AUX_OBJ
+}
 
 export const normalizeDocs = (docs = []) =>
   docs?.map((doc) => normalizeDoc(doc))
+export const datesToFirebaseFromat = ({ document = null }) => {
+  const AUX_DOCUMENT = {}
 
-export const datesToFirebaseFromat = ({
-  birth,
-  registryDate,
-  createdAt,
-  date,
-  updatedAt
-}) => {
-  const foramtedDates = {}
-  if (birth) foramtedDates.birth = dateToFirebaseFormat(birth)
-  if (date) foramtedDates.date = dateToFirebaseFormat(date)
-  if (createdAt) foramtedDates.createdAt = dateToFirebaseFormat(createdAt)
-  if (updatedAt) foramtedDates.updatedAt = dateToFirebaseFormat(updatedAt)
-  if (lastUpdate) foramtedDates.lastUpdate = dateToFirebaseFormat(lastUpdate)
-  if (registryDate)
-    foramtedDates.registryDate = dateToFirebaseFormat(registryDate)
-  return foramtedDates
+  if (!document) return 'no document'
+  if (typeof document !== 'object') {
+    return 'is not an object'
+  }
+  Object.keys(document).forEach((key) => {
+    AUX_DOCUMENT[key] = document[key]
+
+    if (DATE_FIELDS.includes(key)) {
+      AUX_DOCUMENT[key] = new Date(AUX_DOCUMENT[key]).toString()
+    }
+
+    if (typeof document[key] === 'object') {
+      // HAZLO RECURSIVO
+      AUX_DOCUMENT[key] = datesToFirebaseFromat({
+        document: document[key]
+      })
+    }
+    if (DATE_FIELDS.includes(key)) {
+      const aux = dateToFirebaseFormat(document[key])
+      AUX_DOCUMENT[key] = aux
+    }
+  })
+  return AUX_DOCUMENT
 }
 
 export const dateToFirebaseFormat = (date) =>

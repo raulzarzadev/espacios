3d7b8e513b9ac985793f1e6c5eb232b3c4eeb3a7
zasara <raulzarza@gmail.com>
2022-06-06 13:18:34 -0600

    add espacios icon, head

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,10 +4,17 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
 import Layout from 'src/layouts'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+      <title>
+       Espacios App
+      </title>
+      <meta name='description' content=' Espacios App is a platform for the management of spaces to conserve the best environment for your guest and the owners'/>
+    </Head>
       <Provider store={store}>
         <Layout>
           <Component {...pageProps} />
